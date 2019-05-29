27e125886367278c4828734ff4095757e86c4fa2
ljZhang416 <zlj416461712@qq.com>
2019-05-29 16:56:18 +0800

    Update ex10.py

--- lpthw/ex10.py
+++ lpthw/ex10.py
@@ -8,5 +8,3 @@ print('7.Amy got up late, \fand she said "I miss the breakfast,this monring"')#\
 print('8.Amy got up late, \nand she said "I miss the breakfast,this monring"')#\n是换行符
 print('9.Amy got up late, \rand she said "I miss the breakfast,this monring"')#\r是回车
 print('10.Amy got up late, \vand she said "I miss the breakfast,this monring"')#\v是垂直制表符
-print('11.Amy got up late, \012and she said "I miss the breakfast,this monring"')#\\是单斜杠
-
