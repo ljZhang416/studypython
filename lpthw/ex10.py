dd0652cf0a931f1091f8356fad9402c3a559ec8d
ljZhang416 <zlj416461712@qq.com>
2019-05-29 16:35:45 +0800

    Create ex10.py

---
+++ lpthw/ex10.py
@@ -0,0 +1,12 @@
+print('\t1.Amy got up late, and she said "I miss the breakfast,this monring"')#\t是tab
+print('2.Amy got up late, and she said "I miss the breakfast,this monring"')#\\是单斜杠
+print("3.Amy got up late, and she said \"I miss the breakfast,this monring\"")#\"是双引号
+print('4.Amy got up late, and she said \'I miss the breakfast,this monring\'')#\\是单引号
+print('5.Amy got up late, \aand she said "I miss the breakfast,this monring"')#\a是响铃符
+print('6.Amy got up late, \band she said "I miss the breakfast,this monring"')#\b是退格符
+print('7.Amy got up late, \fand she said "I miss the breakfast,this monring"')#\f是换页
+print('8.Amy got up late, \nand she said "I miss the breakfast,this monring"')#\n是换行符
+print('9.Amy got up late, \rand she said "I miss the breakfast,this monring"')#\r是回车
+print('10.Amy got up late, \vand she said "I miss the breakfast,this monring"')#\v是垂直制表符
+print('11.Amy got up late, \012and she said "I miss the breakfast,this monring"')#\\是单斜杠
+
