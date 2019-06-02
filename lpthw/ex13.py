08bd7be7099633a9932a1412d4645848c3d738ec
ljZhang <zlj416461712@qq.com>
2019-06-02 22:40:20 +0800

    Create ex13.py

---
+++ lpthw/ex13.py
@@ -0,0 +1,7 @@
+from sys import argv
+
+pyname, one, two, three = argv
+print("python file name is :", pyname)
+print("The first word is :", one)
+print("The second word is :", two)
+print("The third word is :", three)
