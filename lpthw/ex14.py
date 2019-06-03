9897c56f6ab5ff8c84b8b70f52e30ffa938be2d3
ljZhang <zlj416461712@qq.com>
2019-06-03 22:36:10 +0800

    Create ex14.py

---
+++ lpthw/ex14.py
@@ -0,0 +1,13 @@
+from sys import argv
+
+pyfile, name = argv
+a = "---"
+
+print(f"python file name is :{pyfile}")
+print(f"His name is :{name}")
+
+myname = input("So what's your name?")
+print(f"OK, your name is :{myname}")
+
+hername = input(a)
+print(f"Her name is :{hername}")
