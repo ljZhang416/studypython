a7b644cdb6d5d70a653d93ec3a5a6c4619803e42
ljZhang416 <zlj416461712@qq.com>
2019-07-11 18:03:05 +0800

    Update hw3_1_3.py

--- lpthw/hw3_1_3.py
+++ lpthw/hw3_1_3.py
@@ -2,6 +2,13 @@
 
 # 定义一个函数，可以用来求任意三个数的乘积
 def mul_3(a,b,c):
-    print(a, "*",b,"*",c,"=",(a*b*c))
+    print(a, "*", b, "*", c, "=", (a * b * c) )
 
 mul_3(2,4,5)
+
+
+# 定义一个函数，可以根据不同的用户名显示不同的欢迎信息    
+def name_welcome(name):
+    print(name, "欢迎光临！")
+
+name_welcome("Amy")
