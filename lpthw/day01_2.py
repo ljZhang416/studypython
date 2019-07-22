55ac749a2bf5ca4eb1e600c7a618dd62a0562b1b
ljZhang416 <zlj416461712@qq.com>
2019-07-22 18:39:14 +0800

    Create day01_2.py

---
+++ lpthw/day01_2.py
@@ -0,0 +1,17 @@
+
+a = "5000"
+b = "5000"
+# if a == b:
+# 	print("a==b")
+# else:
+# 	print("a不等于b")
+# print("----------------------------")
+# if a is b:
+# 	print("a 是 b")
+# else:
+# 	print("a不是b")
+# print("----------------------------")
+c = 100
+d = c
+c = c + 1
+print(d)
